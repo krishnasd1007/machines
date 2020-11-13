9fa8d37f82b5ff01eb560f69d26fb04f700ddd8d
Rishabh Verma <rvstowrn@gmail.com>
2020-11-13 22:09:15 +0530

    status check

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,20 @@ class AppBase extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dr Moovy',
-      initialRoute: '/login',
+      initialRoute: '/status',
       routes: {
         '/login': (context) => LoginPage(),                     // UI [], CODE [], LOGIC []  
         '/splash': (context) => SplashPage(),                   // UI [], CODE [], LOGIC []
-        '/status': (context) => StatusPage(),                   // UI [], CODE [], LOGIC []
+        '/status': (context) => StatusPage(),                   // UI [], CODE [], LOGIC [] left
         '/menu': (context) => MenuPage(),                       // UI [], CODE [], LOGIC []
         '/suggestion': (context) => SuggestionPage(),           // UI [], CODE [], LOGIC []
         '/feedback': (context) => FeedbackPage(),               // UI [], CODE [], LOGIC []
-        '/machines': (context) => MachinesPage(),               // UI [], CODE [], LOGIC []
-        '/machinedetails': (context) => MachineDetailsPage(),   // UI [], CODE [], LOGIC []
+        '/machines': (context) => MachinesPage(),               // UI [], CODE [], LOGIC [] half
+        '/machinedetails': (context) => MachineDetailsPage(),   // UI [], CODE [], LOGIC [] 
         '/info': (context) => InfoPage(),                       // UI [], CODE [], LOGIC []
-        '/team': (context) => TeamPage(),                       // UI [], CODE [], LOGIC []
-        '/member': (context) => MemberPage(),                   // UI [], CODE [], LOGIC []
-        '/control': (context) => ControlPage(),                 // UI [], CODE [], LOGIC []
+        '/team': (context) => TeamPage(),                       // UI [], CODE [], LOGIC [] half
+        '/member': (context) => MemberPage(),                   // UI [], CODE [], LOGIC [] 
+        '/control': (context) => ControlPage(),                 // UI [], CODE [], LOGIC [] left
       },
     );
   }
