571de42584c1a641d4158c6ad72497a5473eeed5
Rishabh Verma <rvstowrn@gmail.com>
2020-11-15 15:54:32 +0530

    structuring and compressing code

--- lib/pages/machines.dart
+++ lib/pages/machines.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../machineList.dart';
 
-
 class MachinesPage extends StatefulWidget {
   @override
   _MachinesPageState createState() => _MachinesPageState();
@@ -60,7 +59,6 @@ class _MachinesPageState extends State<MachinesPage> {
     );
   }
 
-
   Widget machineText(str,size,space){
     return Text(str,
       style: TextStyle(
@@ -86,7 +84,7 @@ class _MachinesPageState extends State<MachinesPage> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children:[
-          machineText(machineObj["name"],15,.3),          
+          machineText(machineObj["name"],14,0),          
           SizedBox(height:10),
           RaisedButton(
             color: Colors.greenAccent[400],
@@ -109,12 +107,6 @@ class _MachinesPageState extends State<MachinesPage> {
     );
   }
 
-  Widget singleRow(rowChildren){
-    return Row(
-      children: rowChildren,
-    );
-  }
-
   List<Widget> createMachineLayout(){
     int elementsInRow = (screenMode == "portrait")? 2 : 4;
     int listLength = machineList.length;
@@ -128,21 +120,10 @@ class _MachinesPageState extends State<MachinesPage> {
         rowChildren.add(machineWidget(machineList[index]));
         index++;
       }
-      machineLayout.add(singleRow(rowChildren));
+      machineLayout.add(Row(children: rowChildren));
     }
     return machineLayout;
   } 
-
-
-  Widget base(){
-    return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child:Column(
-        children:createMachineLayout(),
-      )
-    );
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -150,183 +131,14 @@ class _MachinesPageState extends State<MachinesPage> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(55, 55, 55, 1),
       body: SafeArea(
-        child: base(),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(30,5,30,20),
+          child:Column(
+            children:createMachineLayout(),
+          ),
+        ),
       ),
     );
   }
+
 }
-
-
-
-
-
-
-
-
-
-
-// KRISHNA'S CODE
-
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-
-
-// class MachinesPage extends StatefulWidget {
-//   @override
-//   _MachinesPageState createState() => _MachinesPageState();
-// }
-
-// class _MachinesPageState extends State<MachinesPage> {
-
-//   var hl,hrp,wrp,hrl,wrl,ht,wt,hp,wp;
-
-//   Widget button() {
-//     return Material(
-//       color: Colors.blueGrey[900],
-//       child: IconButton(
-//         icon: Icon(Icons.keyboard_arrow_left),
-//         color: Colors.lightGreen,
-//         splashColor: Colors.red,
-//         padding: EdgeInsets.all(10),
-//         onPressed: () {},
-//       ),
-//     );
-//   }
-
-//   Widget raisebutton(st,hr,wr,col) {
-//     return Container(
-//         height: hr,
-//         width: wr,
-//         decoration: BoxDecoration(
-//           borderRadius: BorderRadius.all(Radius.circular(15.0)),
-//         ),
-//         child: RaisedButton(
-//           onPressed: () {},
-//           child: Text(st, style: TextStyle(
-//             color: Colors.white70,
-//             fontWeight: FontWeight.bold,
-//           ),),
-//           color: col,
-//           shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.all(Radius.circular(15.0)),
-//           ),
-//         ),
-//     );
-//   }
-
-//   Widget machine(s,h,w,hr,wr) {
-//     // return 
-//     // Material(
-//       // child: Card(
-//         return Container(
-//           padding: EdgeInsets.all(10.0),
-//           margin: EdgeInsets.all(10.0),
-//           height: h,
-//           width: w,
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.all(Radius.circular(20.0)),
-//             color: Colors.teal[800],
-//             boxShadow: [BoxShadow(color: Colors.black,blurRadius: 5.2,),]
-//           ),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: <Widget>[
-//               Text(s, style: TextStyle(
-//                 color: Colors.white70,
-//                 fontWeight: FontWeight.bold,
-//               ),),
-//               raisebutton('GO',hr,wr,Colors.greenAccent[400]),
-//               raisebutton('DETAILS',hr,wr,Colors.green[700]),
-//             ],
-//           ),
-//         // ),
-//       // ),
-//     );
-//   }
-
-//   Widget row(m1,m2,h,hr,wr) {
-//     return Material(
-//       color: Colors.blueGrey[900],
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: <Widget>[
-//           machine(m1,h,wp,hr,wr),
-//           machine(m2,h,wp,hr,wr),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget appbar(ha,wa) {
-//     return Container(
-//       child: Card(
-//         color: Colors.teal[800],
-//         shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.all(Radius.circular(20.0))
-//         ),
-//         child: Container(
-//           height: ha,
-//           width: wa,
-//           child: Center(
-//             child: Text('MACHINES', style: TextStyle(
-//                 color: Colors.white70, fontWeight: FontWeight.bold,fontSize: ht*0.03)),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget portrait(h,ha,wa,hr,wr) {
-//     return Material(
-//       child: Container(
-//         height: ht,
-//         width: wt,
-//         color: Colors.blueGrey[900],
-//         child: SingleChildScrollView(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: <Widget>[
-//               Padding(padding: EdgeInsets.all(10)),
-//               button(),
-//               appbar(ha, wa),
-//               row('MACHINE 1', 'MACHINE 2', h,hr,wr),
-//               row('MACHINE 3', 'MACHINE 4', h,hr,wr),
-//               row('MACHINE 5', 'MACHINE 6', h,hr,wr),
-//               row('MACHINE 7', 'MACHINE 8', h,hr,wr),
-//             ],
-//           ),
-//         ),
-//       )
-//     );
-//   }
-
-//   Widget landscape() {
-//     return Material(
-//       child: portrait(hl,ht*0.2,wt*0.9,hrl,wrl),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//      ht = MediaQuery.of(context).size.height;
-//      wt = MediaQuery.of(context).size.width;
-//      hp = 0.2*ht;
-//      wp = 0.4*wt;
-//      hl = 0.4*ht;
-//      hrp = 0.05*ht;
-//      wrp = 0.25*wt;
-//      hrl = 0.1*ht;
-//      wrl = 0.25*wt;
-//     return MaterialApp(
-//       home: OrientationBuilder(
-//         builder: (context,orientation) {
-//           if (orientation == Orientation.portrait){
-//             return portrait(hp,ht*0.1,wt*0.9,hrp,wrp);
-//           }else{
-//             return landscape();
-//           }
-//         },
-//       ),
-//     );
-//   }
-// }
